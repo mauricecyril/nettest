f6881a8f5972bb27674c3aca3636493274ac11dc
mauricecyril <25261554+mauricecyril@users.noreply.github.com>
2018-05-01 10:49:18 -0400

    Update nettest.py

--- nettest.py
+++ nettest.py
@@ -13,83 +13,79 @@ import datetime
 import time 
 
 
-def test():
+def speedtest():
 
         #run speedtest-cli
         print('Running Test')
         a = os.popen("speedtest-cli --simple").read()
-        #b = str(a)
-        #print(b)
         print('Test Completed')
 
 
         #split the 3 line result (ping,down,up)
         lines = a.split('\n')
         print(a)
-        #print(lines[0])
         ts = time.time() 
         date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') 
 
         #if speedtest could not connect set the speeds to 0 
         if "Cannot" in a:
-                print("Failed Connection") 
-                #print(a) 
+                msg = str("Failed Connection")
                 p = 0 
                 d = 0 
                 u = 0 
-                print(date, p, d, u) 
+                print(msg, date, p, d, u) 
 
                 #save the data to file for local network plotting 
                 out_file = open('/sdcard/python3/data.csv', 'a') 
                 writer = csv.writer(out_file) 
-                writer.writerow((date,ts*1000,p,d,u)) 
+                writer.writerow((date,msg,p,d,u)) 
                 out_file.close()
 
         #if speedtest connection reset by peer, set the speeds to 0 
         elif "Errno 104" in a:
-                print("Connection Reset by Peer")
+                msg = str("Connection Reset by Peer")
                 p = 0
                 d = 0
                 u = 0
-                print(date, p, d, u)
+                print(msg, date, p, d, u)
 
                 #save the data to file for local network plotting 
                 out_file = open('/sdcard/python3/data.csv', 'a') 
                 writer = csv.writer(out_file) 
-                writer.writerow((date,ts*1000,p,d,u)) 
+                writer.writerow((date,msg,p,d,u)) 
                 out_file.close()
                 
         #if speedtest Times out set the speeds to 0 
         elif "Errno 110" in a:
-                print("Connection Timed Out")
+                msg = str("Connection Timed Out")
                 p = 0
                 d = 0
                 u = 0
-                print(date, p, d, u)
+                print(msg, date, p, d, u)
 
                 #save the data to file for local network plotting 
                 out_file = open('/sdcard/python3/data.csv', 'a') 
                 writer = csv.writer(out_file) 
-                writer.writerow((date,ts*1000,p,d,u)) 
+                writer.writerow((date,msg,p,d,u)) 
                 out_file.close()
 
                 
         else:
-                print("Successful Connection") 
+                msg = str("Successful Connection") 
                 p = lines[0][6:11] 
                 d = lines[1][10:14] 
                 u = lines[2][8:12] 
-                print(date,p, d, u) 
+                print(msg, date, p, d, u) 
 
                 #save the data to file for local network plotting 
                 out_file = open('/sdcard/python3/data.csv', 'a') 
                 writer = csv.writer(out_file) 
-                writer.writerow((date,ts*1000,p,d,u))
+                writer.writerow((date,msg,p,d,u))
                 out_file.close()
 
         return
 
 
 if __name__ == '__main__':
-        test()
-        print('Script Completed')
+        speedtest()
+        print("Script Completed and Results Logged")
